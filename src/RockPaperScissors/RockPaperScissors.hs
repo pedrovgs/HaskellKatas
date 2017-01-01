8605777f2421d320dbbb996e0eda4922d96d2578
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-01 18:40:51 +0100

    Kata finished

--- src/RockPaperScissors/RockPaperScissors.hs
+++ src/RockPaperScissors/RockPaperScissors.hs
@@ -1,8 +1,8 @@
-module RockPaperScissors where
+module RockPaperScissors.RockPaperScissors where
 
-data Gesture = Rock | Paper | Scissors deriving (Enum)
-
+data Gesture = Rock | Paper | Scissors deriving (Eq, Enum, Show)
 data Player = Player String Gesture
+data Result = Player1Wins | Player2Wins | Draw deriving (Eq, Enum, Show)
 
 name :: Player -> String
 name (Player name _ ) = name
@@ -10,5 +10,11 @@ name (Player name _ ) = name
 gesture :: Player -> Gesture
 gesture (Player _ gesture) = gesture
 
-whoWins :: Player -> Player-> Gesture
-whoWins player1 player2 = undefined
+whoWins :: Player -> Player -> Result
+whoWins (Player _ Rock) (Player _ Scissors) = Player1Wins
+whoWins (Player _ Scissors) (Player _ Paper) = Player1Wins
+whoWins (Player _ Paper) (Player _ Rock) = Player1Wins
+whoWins (Player _ Rock) (Player _ Paper) = Player2Wins
+whoWins (Player _ Paper) (Player _ Scissors) = Player2Wins
+whoWins (Player _ Scissors) (Player _ Rock) = Player2Wins
+whoWins _ _ = Draw
