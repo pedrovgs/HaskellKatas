53fd7fe07297dff638b6d6cc281b9e5903e792ee
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-03 19:42:24 +0100

    Refactor Potter.hs implementation to be easier to read

--- src/Potter/Potter.hs
+++ src/Potter/Potter.hs
@@ -1,22 +1,25 @@
-module Potter.Potter where
+module Potter.Potter
+( HarryPotterBook(..)
+, calculatePrice
+, oneBookPrice
+)where
 
 import           Data.List
 
 data HarryPotterBook = Book1 | Book2 | Book3 | Book4 | Book5 deriving (Enum, Bounded, Show, Eq)
 
 calculatePrice :: [HarryPotterBook] -> Float
-calculatePrice []  = 0.0
-calculatePrice [x] = 8.0
-calculatePrice cart = calculatePriceWithDiscount $ map length (group cart)
+calculatePrice books = calculatePriceInner groupedBooks
+  where groupedBooks = map length (group books)
 
-calculatePriceWithDiscount :: [Int] -> Float
-calculatePriceWithDiscount groupedBooks
+calculatePriceInner :: [Int] -> Float
+calculatePriceInner groupedBooks
   | differentBooks == 0 = 0
   | differentBooks == 1 = oneBookPrice * fromIntegral (head groupedBooks)
-  | differentBooks == 2 = oneBookPrice * 2.0 * 0.95 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
-  | differentBooks == 3 = oneBookPrice * 3.0 * 0.90 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
-  | differentBooks == 4 = oneBookPrice * 4.0 * 0.80 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
-  | differentBooks == 5 = oneBookPrice * 5.0 * 0.75 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
+  | differentBooks == 2 = oneBookPrice * 2.0 * 0.95 + calculatePriceInner (removeBooksFromCart groupedBooks)
+  | differentBooks == 3 = oneBookPrice * 3.0 * 0.90 + calculatePriceInner (removeBooksFromCart groupedBooks)
+  | differentBooks == 4 = oneBookPrice * 4.0 * 0.80 + calculatePriceInner (removeBooksFromCart groupedBooks)
+  | differentBooks == 5 = oneBookPrice * 5.0 * 0.75 + calculatePriceInner (removeBooksFromCart groupedBooks)
   where differentBooks = length groupedBooks
 
 oneBookPrice = 8.0
