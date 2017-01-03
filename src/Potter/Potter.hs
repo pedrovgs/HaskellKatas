9408f2341aa6e8692ded4ad703a9c9da5455a398
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-03 16:02:50 +0100

    Prepare spec, modules and readme links for our next Kata

---
+++ src/Potter/Potter.hs
@@ -0,0 +1,1 @@
+module Potter.Potter where
---
+++ test/Potter/PotterSpec.hs
@@ -0,0 +1,10 @@
+module Potter.PotterSpec where
+
+import           Test.Hspec
+import           Test.QuickCheck
+
+spec = describe "Potter requirements" $ do
+  it "test" $
+    True `shouldBe` True
+  it "test 2" $
+    True `shouldBe` True
