b66f05eb5f334d36b140f3d30cd8fe62e63285e3
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-03 16:28:46 +0100

    Create the first two scenarios described in the kata where there are no books or just one book in the cart

--- src/Potter/Potter.hs
+++ src/Potter/Potter.hs
@@ -1,1 +1,7 @@
 module Potter.Potter where
+
+data HarryPotterBook = Book1 | Book2 | Book3 | Book4 | Book5 deriving (Enum, Bounded, Show)
+
+calculatePrice :: [HarryPotterBook] -> Float
+calculatePrice [] = 0.0
+calculatePrice [x] = 8.0
--- test/Potter/PotterSpec.hs
+++ test/Potter/PotterSpec.hs
@@ -1,10 +1,18 @@
 module Potter.PotterSpec where
 
+import           Potter.Potter
 import           Test.Hspec
 import           Test.QuickCheck
 
 spec = describe "Potter requirements" $ do
-  it "test" $
-    True `shouldBe` True
-  it "test 2" $
-    True `shouldBe` True
+  it "an empty cart costs 0 EUR" $
+    calculatePrice [] == 0.0
+  it "just one book costs 8 EUR" $
+    property prop_BuyJustOneBookCostsEightEuros
+
+
+prop_BuyJustOneBookCostsEightEuros :: Property
+prop_BuyJustOneBookCostsEightEuros = forAll harryPotterBook (\book  -> calculatePrice [book] == 8.0)
+
+harryPotterBook :: Gen HarryPotterBook
+harryPotterBook = elements [(minBound::HarryPotterBook) .. (maxBound::HarryPotterBook)]
