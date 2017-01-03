c98bb75215eba5934d722d0ffc328ea55125d53a
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-03 19:35:26 +0100

    Specs related with duplicated books finished

--- src/Potter/Potter.hs
+++ src/Potter/Potter.hs
@@ -1,7 +1,26 @@
 module Potter.Potter where
 
-data HarryPotterBook = Book1 | Book2 | Book3 | Book4 | Book5 deriving (Enum, Bounded, Show)
+import           Data.List
+
+data HarryPotterBook = Book1 | Book2 | Book3 | Book4 | Book5 deriving (Enum, Bounded, Show, Eq)
 
 calculatePrice :: [HarryPotterBook] -> Float
-calculatePrice [] = 0.0
+calculatePrice []  = 0.0
 calculatePrice [x] = 8.0
+calculatePrice cart = calculatePriceWithDiscount $ map length (group cart)
+
+calculatePriceWithDiscount :: [Int] -> Float
+calculatePriceWithDiscount groupedBooks
+  | differentBooks == 0 = 0
+  | differentBooks == 1 = oneBookPrice * fromIntegral (head groupedBooks)
+  | differentBooks == 2 = oneBookPrice * 2.0 * 0.95 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
+  | differentBooks == 3 = oneBookPrice * 3.0 * 0.90 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
+  | differentBooks == 4 = oneBookPrice * 4.0 * 0.80 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
+  | differentBooks == 5 = oneBookPrice * 5.0 * 0.75 + calculatePriceWithDiscount (removeBooksFromCart groupedBooks)
+  where differentBooks = length groupedBooks
+
+oneBookPrice = 8.0
+
+removeBooksFromCart :: [Int] -> [Int]
+removeBooksFromCart groupedBooks = filter (>0) updatedGroups
+  where updatedGroups = map (\x -> x - 1) groupedBooks
