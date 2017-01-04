ba6020a2aad30a3b651145296d97643d871b58e5
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-04 15:52:56 +0100

    Finish the first part of this kata

--- src/RomanNumerals/RomanNumerals.hs
+++ src/RomanNumerals/RomanNumerals.hs
@@ -1,1 +1,42 @@
-module RomanNumerals.RomanNumerals where
+module RomanNumerals.RomanNumerals
+( RomanNumeral(..)
+, toRoman)
+where
+
+data RomanNumeral =  I | IV | V | IX | X | L | XC | C | D | CM | M
+  deriving (Enum, Eq, Ord, Bounded, Show)
+
+toRoman :: Int -> [RomanNumeral]
+toRoman n = toRomanInner n []
+
+-- toArabic
+
+toRomanInner :: Int -> [RomanNumeral] -> [RomanNumeral]
+toRomanInner 0 acc = acc
+toRomanInner n acc = toRomanInner (n - closerRomanValue) (acc++[closerRoman])
+  where closerRoman = extractCloserRoman n
+        closerRomanValue = romanValue closerRoman
+
+extractCloserRoman :: Int -> RomanNumeral
+extractCloserRoman n = last $ takeWhile (`canExtract` n) romanNumerals
+
+canExtract :: RomanNumeral -> Int -> Bool
+canExtract roman n = n - romanValue roman >= 0
+
+romanNumerals :: [RomanNumeral]
+romanNumerals =  [minValue..maxValue]
+  where maxValue = maxBound::RomanNumeral
+        minValue = minBound::RomanNumeral
+
+romanValue :: RomanNumeral -> Int
+romanValue I  = 1
+romanValue IV = 4
+romanValue V  = 5
+romanValue IX = 9
+romanValue X  = 10
+romanValue L  = 50
+romanValue XC = 90
+romanValue C  = 100
+romanValue D  = 500
+romanValue CM = 900
+romanValue M  = 1000
--- test/RomanNumerals/RomanNumeralsSpec.hs
+++ test/RomanNumerals/RomanNumeralsSpec.hs
@@ -5,5 +5,15 @@ import           Test.Hspec
 import           Test.QuickCheck
 
 spec = describe "RomanNumerals requirements" $ do
-    it "returns 0 if the input is empty" $
-      True == True
+  it "translates 1 as I" $
+    toRoman 1 == [I]
+  it "translates 3 as III" $
+    toRoman 3 == [I,I,I]
+  it "translates 5 as V" $
+    toRoman 5 == [V]
+  it "translates 0 as empty" $
+    null $ toRoman 0
+  it "translates 100 as C" $
+    toRoman 100 == [C]
+  it "translates 1990 as MCMXC" $
+    toRoman 1990 == [M, CM, XC]
