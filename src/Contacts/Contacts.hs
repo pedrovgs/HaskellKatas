0099a4773f018542235a44810a93939e901849b6
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-06 20:26:00 +0100

    Create empty module, empty spec and update readme with the kata statement

---
+++ src/Contacts/Contacts.hs
@@ -0,0 +1,3 @@
+module Contacts.Contacts
+()
+where
---
+++ test/Contacts/ContactsSpec.hs
@@ -0,0 +1,9 @@
+module Contacts.ContactsSpec where
+
+import Contacts.Contacts
+import Test.Hspec
+import Test.QuickCheck
+
+spec = describe "Contacts requirements" $ do
+  it "test" $
+    True == True
