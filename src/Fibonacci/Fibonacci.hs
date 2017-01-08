9f3dd5dc76cce220ca646fa8b8b1344074ad3553
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-01-08 20:16:20 +0100

    Migrate Fibonacci kata to use Maybe Integer as result. We will return Nothing if the value used as input is lower than n

--- src/Fibonacci/Fibonacci.hs
+++ src/Fibonacci/Fibonacci.hs
@@ -2,13 +2,18 @@ module Fibonacci.Fibonacci
 (fibonacci
 , fibonacciTailRec) where
 
-fibonacci :: Integer -> Integer
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+fibonacci :: Integer -> Maybe Integer
+fibonacci 0 = Just 0
+fibonacci 1 = Just 1
+fibonacci n = if n < 0 then Nothing
+              else do a <- fibonacci (n - 1)
+                      b <- fibonacci (n - 2)
+                      Just (a + b)
 
-fibonacciTailRec :: Integer -> Integer
-fibonacciTailRec n = fibonacciTailRecInner n 1 0
+fibonacciTailRec :: Integer -> Maybe Integer
+fibonacciTailRec n
+  | n < 0 = Nothing
+  | otherwise = Just (fibonacciTailRecInner n 1 0)
 
 fibonacciTailRecInner :: Integer -> Integer -> Integer -> Integer
 fibonacciTailRecInner 0 _ b = b
--- test/Fibonacci/FibonacciSpec.hs
+++ test/Fibonacci/FibonacciSpec.hs
@@ -1,32 +1,23 @@
 module Fibonacci.FibonacciSpec where
 
+import           Data.Maybe
 import           Fibonacci.Fibonacci
 import           Test.Hspec
 import           Test.QuickCheck
 
 spec = describe "Fibonacci requirements" $ do
   it "tail recursive and regular implementation of the fibonacci sequence returns the same value" $
-    quickCheckWith stdArgs { maxSuccess = 20 }  $ forAll smallValues (\n -> fibonacci n == fibonacciTailRec n)
-  it "non tail recursive implementation" $
-    quickCheckWith stdArgs { maxSuccess = 5 } prop_NonTailRecursiveFibonacci
-  it "tail recursive implementation" $
-    quickCheckWith stdArgs { maxSuccess = 200 } prop_TailRecursiveFibonacci
+    verboseCheckWith stdArgs { maxSuccess = 20 }  $ forAll smallValues (\n -> fibonacci n == fibonacciTailRec n)
 
-prop_NonTailRecursiveFibonacci :: Property
-prop_NonTailRecursiveFibonacci =
-  forAll smallValues
-    (\n -> let x = fibonacci n
-               y = fibonacci (n - 1)
-               z = fibonacci (n - 2)
-           in x == y + z)
+  it "tail recursive implementation" $
+    verboseCheckWith stdArgs { maxSuccess = 200 } prop_TailRecursiveFibonacci
 
 prop_TailRecursiveFibonacci :: Property
 prop_TailRecursiveFibonacci =
-  forAll mediumValues
-    (\n -> let x = fibonacciTailRec n
-               y = fibonacciTailRec (n - 1)
-               z = fibonacciTailRec (n - 2)
-           in x == y + z)
+  forAll mediumValues $ \n -> fromJust $ do x <- fibonacciTailRec n
+                                            y <- fibonacciTailRec (n - 1)
+                                            z <- fibonacciTailRec (n - 2)
+                                            return (x == y + z)
 
 positive :: Gen Integer
 positive = do n <- arbitrary
